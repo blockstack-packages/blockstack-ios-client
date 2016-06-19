b29d4fce40ddffe5a58dfab19907d2c8c0d25a0b
Jorge Tapia <jorge@profapps.co>
2016-06-19 00:38:36 -0400

    Add base project files.

---
+++ blockstack-client/blockstack-client.h
@@ -0,0 +1,19 @@
+//
+//  blockstack-client.h
+//  blockstack-client
+//
+//  Created by Jorge Tapia on 6/19/16.
+//  Copyright © 2016 Blockstack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for blockstack-client.
+FOUNDATION_EXPORT double blockstack_clientVersionNumber;
+
+//! Project version string for blockstack-client.
+FOUNDATION_EXPORT const unsigned char blockstack_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <blockstack_client/PublicHeader.h>
+
+
---
+++ blockstack-clientTests/blockstack_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  blockstack_clientTests.swift
+//  blockstack-clientTests
+//
+//  Created by Jorge Tapia on 6/19/16.
+//  Copyright © 2016 Blockstack. All rights reserved.
+//
+
+import XCTest
+@testable import blockstack_client
+
+class blockstack_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
