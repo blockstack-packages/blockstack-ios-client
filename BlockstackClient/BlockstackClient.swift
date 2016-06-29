2ec489cf9b1bf7321b572e17a9d92069128e6529
Jorge Tapia <jorge@profapps.co>
2016-06-28 20:42:26 -0400

    Fix documentation text.

--- BlockstackClient/BlockstackClient.swift
+++ BlockstackClient/BlockstackClient.swift
@@ -30,7 +30,7 @@ public struct BlockstackClient {
         self.appSecret = appSecret
     }
     
-    /// Detemines if the app id and app secret are set and valid.
+    /// Determines if the app id and app secret are set and valid.
     ///
     /// - Returns: Boolean value indicating wether the client is valid or not.
     private static func clientIsValid() -> Bool {
