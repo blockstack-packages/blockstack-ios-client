ab576c1438c1ecbe7d71429d9f671dcdb75daec0
Jorge Tapia <jorge@profapps.co>
2016-08-25 20:59:56 -0400

    Complete user operations

--- Blockstack/Blockstack.swift
+++ Blockstack/Blockstack.swift
@@ -61,4 +61,102 @@ extension Blockstack {
         }
     }
     
+    /// Takes in a search query and returns a list of results that match the search.
+    /// The query is matched against +usernames, full names, and twitter handles by default.
+    /// It's also possible to explicitly search verified Twitter, Facebook, Github accounts, and verified domains.
+    /// This can be done by using search queries like twitter:itsProf, facebook:g3lepage, github:shea256, domain:muneebali.com
+    ///
+    /// - Parameters:
+    ///     - query: The text to search for.
+    ///     - completion: Closure containing an array of results, where each result has a "profile" object or an error.
+    public func search(query query: String, completion: (response: AnyObject?, error: NSError?) -> Void) {
+        let searchEndpoint = "\(Endpoints.search)\(query)"
+        
+        if let authorizationValue = getAuthorizationValue() {
+            let headers = ["Authorization": authorizationValue]
+            
+            Alamofire.request(.GET, searchEndpoint, headers: headers).responseJSON { response in
+                completion(response: response.data, error: response.result.error)
+            }
+        }
+    }
+    
+    /// Registers a username.
+    ///
+    /// - Parameters:
+    ///     - username: The username to be registered.
+    ///     - recipientAddress: Bitcoin address of the new owner address.
+    ///     - profileData: The data to be associated with the blockchain ID.
+    ///     - completion: Closure a response that could include an object with an unsigned transaction "unsigned_tx" in hex format.
+    public func registerUser(username: String, recipientAddress: String, profileData: [String: AnyObject]?, completion: (response: AnyObject?, error: NSError?) -> Void) {
+        if let authorizationValue = getAuthorizationValue() {
+            let headers = ["Authorization": authorizationValue]
+            
+            var params: [String: AnyObject] = ["username": username, "recipient_address": recipientAddress]
+            
+            if let profile = profileData {
+                params["profile"] = profile
+            }
+            
+            Alamofire.request(.POST, Endpoints.users, parameters: params, encoding: .JSON, headers: headers).responseJSON { response in
+                completion(response: response.data, error: response.result.error)
+            }
+        }
+    }
+    
+    /// Updates a username.
+    ///
+    /// - Parameters:
+    ///     - username: The username to be updated.
+    ///     - profileData: The data to be associated with the blockchain ID.
+    ///     - ownerPublicKey: Public key of the Bitcoin address that currently owns the username.
+    ///     - completion: Closure with a response that could include an object with an unsigned transaction "unsigned_tx" in hex format.
+    public func updateUser(username: String, profileData: [String: AnyObject], ownerPublicKey: String, completion: (response: AnyObject?, error: NSError?) -> Void) {
+        if let authorizationValue = getAuthorizationValue() {
+            let updateEndpoint = "\(Endpoints.users)/\(username)/update)"
+            
+            let headers = ["Authorization": authorizationValue]
+            let params: [String: AnyObject] = ["profile": profileData, "owner_pubkey": ownerPublicKey]
+            
+            Alamofire.request(.POST, updateEndpoint, parameters: params, encoding: .JSON, headers: headers).responseJSON { response in
+                completion(response: response.data, error: response.result.error)
+            }
+        }
+    }
+    
+    /// Transfers a user to another Bitcoin address.
+    ///
+    /// - Parameters:
+    ///     - username: The username to be transfered.
+    ///     - transferAddress: Bitcoin address of the new owner address.
+    ///     - ownerPublicKey: Public key of the Bitcoin address that currently owns the username.
+    ///     - completion: Closure with a response that could include an object with an unsigned transaction "unsigned_tx" in hex format.
+    public func transferUser(username: String, transferAddress: String, ownerPublicKey: String, completion: (response: AnyObject?, error: NSError?) -> Void) {
+        if let authorizationValue = getAuthorizationValue() {
+            let updateEndpoint = "\(Endpoints.users)/\(username)/update)"
+            
+            let headers = ["Authorization": authorizationValue]
+            let params: [String: AnyObject] = ["transfer_address": transferAddress, "owner_pubkey": ownerPublicKey]
+            
+            Alamofire.request(.POST, updateEndpoint, parameters: params, encoding: .JSON, headers: headers).responseJSON { response in
+                completion(response: response.data, error: response.result.error)
+            }
+        }
+    }
+    
+    /// Returns an object with "stats", and "usernames".
+    /// "stats" is a sub-object which in turn contains a "registrations" field that reflects a running count of the total users registered.
+    /// "usernames" is a list of all usernames in the namespace.
+    ///
+    /// - Parameter completion: Closure with and object that contains "stats" and "usernames" or an error.
+    public func allUsers(completion: (response: AnyObject?, error: NSError?) -> Void) {
+        if let authorizationValue = getAuthorizationValue() {
+            let headers = ["Authorization": authorizationValue]
+            
+            Alamofire.request(.GET, Endpoints.users, headers: headers).responseJSON { response in
+                completion(response: response.data, error: response.result.error)
+            }
+        }
+    }
+    
 }
